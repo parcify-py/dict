824af89d2deeef0f2badda30a3faabd209d970a6
unknown <makar.simonov@infis.cz>
2025-05-06 09:34:32 +0200

    commit

--- test5345.py
+++ test5345.py
@@ -1,17 +1,39 @@
-robots = {'RD2R': 'repair'}
-start = True
 
-while start:
-    robot = input("Enter name of robot: ")
-    if robot in robots:
-        print(f"Function: {robots[robot]}")
-    else:
-        choice = input("Robot not found. Type 'exit'or 'add': ").lower()
-        if choice == 'exit':
-            start = False
-        elif choice == 'add':
-            new_name = input("Enter name of new robot: ")
-            new_func = input("Enter function of new robot: ")
-            robots[new_name] = new_func
+def create_shelter(name,dict):
+    shelter = {name:dict}
+    return shelter 
 
-print(robots)
+def add_animal(shelter: dict, name: str,type: str,old: int):
+    global shelter
+    shelter[name] = {'old':old,'type':type}
+    
+def print_animals():
+    global shelter
+    key = shelter.keys()
+    for i in range(len(shelter)):
+        print(f"{list(key)[i]} is a {shelter[list(key)[i]]['type']}, and it is {shelter[list(key)[i]]['old']} years old")
+        
+def print_animalstype(type):
+    global shelter
+    key = shelter.keys()
+    for i in range(len(shelter)):
+        if shelter[list(key)[i]]['type'] == type:
+            print(list(key)[i])
+
+name = 'Lala'
+lala = {'old':5,'type':'cat'} 
+
+shelter = create_shelter(name,lala)
+
+add_animal('Mdk','dog',8)
+print_animals()
+print_animalstype('cat')
+
+
+
+
+
+
+
+
+

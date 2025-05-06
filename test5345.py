2f7dfb753d2df0fb6072f518d64ae6a5afe14c78
parcify <simonovmakar73@gmail.com>
2025-05-06 09:51:56 +0200

    UPDATTEEEEEEEEE !!!!!!!!

--- test5345.py
+++ test5345.py
@@ -1,39 +1,30 @@
+def create_shelter(first_animal_name,first_animal_age, first_animal_type):
+    return {first_animal_name:{'type':first_animal_type,'age':first_animal_age}}
 
-def create_shelter(name,dict):
-    shelter = {name:dict}
-    return shelter 
+def add_animal(shelter: dict, name: str, type: str, age: int):
+    shelter[name] = {'age': age, 'type': type}
 
-def add_animal(shelter: dict, name: str,type: str,old: int):
-    global shelter
-    shelter[name] = {'old':old,'type':type}
-    
-def print_animals():
-    global shelter
-    key = shelter.keys()
-    for i in range(len(shelter)):
-        print(f"{list(key)[i]} is a {shelter[list(key)[i]]['type']}, and it is {shelter[list(key)[i]]['old']} years old")
-        
-def print_animalstype(type):
-    global shelter
-    key = shelter.keys()
-    for i in range(len(shelter)):
-        if shelter[list(key)[i]]['type'] == type:
-            print(list(key)[i])
+def print_animals(shelter: dict):
+    for name, info in shelter.items():
+        print(f"{name} is a {info['type']}, and it is {info['age']} years old")
 
-name = 'Lala'
-lala = {'old':5,'type':'cat'} 
-
-shelter = create_shelter(name,lala)
-
-add_animal('Mdk','dog',8)
-print_animals()
-print_animalstype('cat')
+def print_animals_by_type(shelter: dict, type):
+    for name, info in shelter.items():
+        if info['type'] == type:
+            print(name)
 
 
+first_animal_name = input("Enter the first animal name: ")
+first_animal_type = input("Enter the animal type: ")
+first_animal_age = int(input("Enter the animal's age: "))
+
+shelter = create_shelter(first_animal_name,first_animal_age, first_animal_type)
 
 
+add_animal(shelter, 'Mdk', 'dog', 8)
 
+print_animals(shelter)
 
-
+print_animals_by_type(shelter, 'cat')
 
 

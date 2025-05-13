b1d176f23ee31a52c5ff532fa894936769d822eb
unknown <makar.simonov@infis.cz>
2025-05-13 09:22:55 +0200

    UPDATE

--- test5345.py
+++ test5345.py
@@ -1,39 +1,24 @@
+films = {"Matrix":2004,"Drive":2010,"Last of Us":2017,"Jumanji":2022}
 
-def create_shelter(name,dict):
-    shelter = {name:dict}
-    return shelter 
+player = {'name':'Adolf','level':'6','inventory':['water','gun','book']}
 
-def add_animal(shelter: dict, name: str,type: str,old: int):
-    global shelter
-    shelter[name] = {'old':old,'type':type}
+player['inventory'].append('key')
+
+def atack(player1=str,player2=str):
+    global characters
+    characters[player1]['hp'] -= characters[player2]['damage']
+    print(f"{player2.capitalize()} atack {player1}\n ")
+
+def print_hp():
+    print(f"Player one hp: {characters['player1']['hp']} Player two hp: {characters['player2']['hp']} \n")
+
     
-def print_animals():
-    global shelter
-    key = shelter.keys()
-    for i in range(len(shelter)):
-        print(f"{list(key)[i]} is a {shelter[list(key)[i]]['type']}, and it is {shelter[list(key)[i]]['old']} years old")
-        
-def print_animalstype(type):
-    global shelter
-    key = shelter.keys()
-    for i in range(len(shelter)):
-        if shelter[list(key)[i]]['type'] == type:
-            print(list(key)[i])
 
-name = 'Lala'
-lala = {'old':5,'type':'cat'} 
-
-shelter = create_shelter(name,lala)
-
-add_animal('Mdk','dog',8)
-print_animals()
-print_animalstype('cat')
+characters = {'player1':{'hp':100,'damage':20,'superpower':['magick','fly','teleport']},'player2':{'hp':120,'damage':15,'superpower':['magick','fly','teleport']}}
 
 
-
-
-
-
-
+print_hp()
+atack('player1','player2')
+print_hp()
 
 
